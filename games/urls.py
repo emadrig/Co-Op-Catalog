8de116a601f5a1c6588a1f3aa6db79728626f727
Kramer Smith <kramersmith41@yahoo.com>
2023-06-14 11:59:51 -0500

    Worked on api_list_games_record

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import api_list_games, api_show_game
+from .views import api_list_games, api_show_game, api_list_games_records
 
 urlpatterns = [
     path("games/", api_list_games, name='api_list_games'),
-    path("games/<str:name>", api_show_game, name="api_show_game"),
+    path("games/<str:name>/", api_show_game, name="api_show_game"),
+    path("gamerecords/", api_list_games_records, name="api_list_games_records"),
 ]
--- games/views.py
+++ games/views.py
@@ -3,8 +3,8 @@ from .models import Game, GamesRecord
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from common.json import ModelEncoder
+from accounts.models import User
 import json
-
 
 class GameEncoder(ModelEncoder):
     model = Game
@@ -15,6 +15,17 @@ class GameEncoder(ModelEncoder):
     ]
     def get_extra_data(self, o):
         return {"gif": str(o.gif)}
+
+class GamesRecordEncoder(ModelEncoder):
+    model = GamesRecord
+    properties = [
+        "score",
+        "game",
+        "player"
+    ]
+    encoders = {
+        "game": GameEncoder
+    }
 
 
 @require_http_methods(["GET", "POST"])
@@ -41,3 +52,25 @@ def api_show_game(request, name):
         {"games": game},
         encoder=GameEncoder
     )
+
+@require_http_methods(["GET", "POST"])
+def api_list_games_records(request):
+    if request.method == "GET":
+        records = GamesRecord.objects.all()
+        return JsonResponse(
+            {"records": records},
+            encoder=GamesRecordEncoder,
+            safe=False
+        )
+    else:
+        content = json.loads(request.body)
+        game = Game.objects.get(id=content['game'])
+        content['game'] = game
+        print(request.user.username, " ______________________________________________")
+        content['player'] = User.objects.get(id=request.user)
+        record = GamesRecord.objects.create(**content)
+        return JsonResponse(
+            {"record": record},
+            encoder=GamesRecordEncoder,
+            safe=False
+        )
