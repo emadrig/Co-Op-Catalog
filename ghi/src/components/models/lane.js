b155ef4db47940f7c89bf3ed3d2b78fa6914aa86
Kramer Smith <kramersmith41@yahoo.com>
2023-08-10 21:01:54 -0500

    Trucks now moving on lanes, lanes are created and deleted as the character moves up and down on the z axis

--- ghi/src/components/models/lane.js
+++ ghi/src/components/models/lane.js
@@ -1,28 +1,49 @@
-import React, { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { Truck } from "./truck";
+import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 
-export function Lane(props) {
-    const { nodes, materials } = useGLTF("/models/lane.gltf")
-    return (
-        <group {...props} dispose={null}>
-            <group name="Lane" position={[-50, 0, 0]} scale={[2, 1, 1]}>
-                <mesh
-                    name="Plane001"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Plane001.geometry}
-                    material={materials.Asphalt}
-                />
-                <mesh
-                    name="Plane001_1"
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Plane001_1.geometry}
-                    material={materials.White}
-                />
-            </group>
-        </group>
-    );
-}
 
-useGLTF.preload("/models/lane.gltf");
+export function Lane(props) {
+  const { nodes, materials } = useGLTF("/models/lane.gltf");
+  const truckRef = useRef();
+  const startTimeRef = useRef(Date.now() + Math.random() * 10 * 1000);
+  const directionRef = useRef(1);
+  const truckSpacing = 5; // Spacing between trucks
+
+  // Function to handle truck movement
+  const moveTruck = () => {
+    const elapsedTime = (Date.now() - startTimeRef.current) / 1000;
+    const xPosition = elapsedTime * 0.5 * directionRef.current; // Change 0.5 to control speed
+    truckRef.current.position.x = (xPosition % truckSpacing) - truckSpacing / 2;
+
+    // You can change direction based on some condition
+    // directionRef.current = someCondition ? 1 : -1;
+  };
+
+  useFrame(moveTruck);
+
+  return (
+    <group {...props} dispose={null}>
+      <group name="Lane" position={[0, 0, 0]} scale={[1, 1, 1]}>
+        <mesh
+          name="Plane001"
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane001.geometry}
+          material={materials.Asphalt}
+        />
+        <mesh
+          name="Plane001_1"
+          castShadow
+          receiveShadow
+          geometry={nodes.Plane001_1.geometry}
+          material={materials.White}
+        />
+        <group ref={truckRef}>
+          <Truck />
+        </group>
+      </group>
+    </group>
+  );
+}
--- ghi/src/components/roadCrossing/RoadCrossing.js
+++ ghi/src/components/roadCrossing/RoadCrossing.js
@@ -6,23 +6,53 @@ import { Lane } from '../models/lane'
 import { Truck } from '../models/truck'
 
 function RoadCrossing() {
-    const [position, setPosition] = useState([0, 1, 0])
-    const models = useGLTF('/models/lane.gltf')
-    console.log(models.scene.children);
-    const { camera } = useThree()
+    const [position, setPosition] = useState([0, 1, 0]);
+    const [lanes, setLanes] = useState([
+        { id: 1, position: [0, 0, 8] },
+        { id: 2, position: [0, 0, 6] },
+        { id: 3, position: [0, 0, 4] },
+        { id: 4, position: [0, 0, 2] },
+        { id: 5, position: [0, 0, 0] },
+        { id: 6, position: [0, 0, -2] },
+        { id: 7, position: [0, 0, -4] },
+        { id: 8, position: [0, 0, -6] },
+        { id: 9, position: [0, 0, -8] },
+        { id: 10, position: [0, 0, -10] },
+        { id: 11, position: [0, 0, -12] },
+        { id: 12, position: [0, 0, -14] },
+        { id: 13, position: [0, 0, -16] },
+        { id: 14, position: [0, 0, -18] },
+        { id: 15, position: [0, 0, -20] },
+        { id: 16, position: [0, 0, -22] },
+    ]);
+    const models = useGLTF("/models/lane.gltf");
+    const lane = models.scene.children[0];
+    const { camera } = useThree();
 
     useEffect(() => {
         const handleKeyDown = (event) => {
-            let newPosition = [...position]; // Copy the current position
+            let newPosition = [...position];
             switch (event.key) {
-                case 'w':
+                case "w":
                     newPosition[2] -= 2;
+                    setLanes((prevLanes) => {
+                        const newLanes = prevLanes.slice(1);
+                        const newLane = { id: new Date().getTime(), position: [0, 0, prevLanes[prevLanes.length - 1].position[2] - 2] }; // Create a new lane
+                        newLanes.push(newLane);
+                        return newLanes;
+                    });
+                    break;
+                case "s":
+                    newPosition[2] += 2;
+                    setLanes((prevLanes) => {
+                        const newLanes = prevLanes.slice(0, -1);
+                        const newLane = { id: new Date().getTime(), position: [0, 0, prevLanes[0].position[2] + 2] }; // Create a new lane
+                        newLanes.unshift(newLane);
+                        return newLanes;
+                    });
                     break;
                 case 'a':
                     newPosition[0] -= 2;
-                    break;
-                case 's':
-                    newPosition[2] += 2;
                     break;
                 case 'd':
                     newPosition[0] += 2;
@@ -32,16 +62,19 @@ function RoadCrossing() {
             }
 
             setPosition(newPosition);
-            camera.position.set(newPosition[0] + 3, newPosition[1] + 3, newPosition[2] + 6);
-        }
+            camera.position.set(
+                newPosition[0] + 1,
+                newPosition[1] + 6,
+                newPosition[2] + 4
+            );
+        };
 
-        window.addEventListener('keydown', handleKeyDown)
+        window.addEventListener("keydown", handleKeyDown);
 
-        // Cleanup function to remove the event listener when the component is unmounted
         return () => {
-            window.removeEventListener('keydown', handleKeyDown)
-        }
-    }, [position, camera])
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [position, camera]);
 
     useFrame(() => {
         camera.lookAt(position[0], position[1], position[2]);
@@ -56,10 +89,12 @@ function RoadCrossing() {
                 <boxGeometry args={[2, 2, 2]} />
                 <meshStandardMaterial />
             </mesh>
-            <Clone object={models.scene.children[0]} />
-            <Clone object={models.scene.children[1]} />
+            {lanes.map((laneItem) => (
+                <Lane key={laneItem.id} object={lane} position={laneItem.position} />
+            ))}
+            <Truck />
         </>
-    )
+    );
 }
 
-export default RoadCrossing
+export default RoadCrossing;
